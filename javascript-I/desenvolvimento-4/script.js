b4d0b4d9ea19c295323f2e3204e876739ac1b07e
Vinícius Luiz A <120275756+vlaskepper@users.noreply.github.com>
2023-10-02 10:56:33 -0300

    desenvolvimento-4

---
+++ javascript-I/desenvolvimento-4/script.js
@@ -0,0 +1,8 @@
+function imprimeConexão() {
+    for (let i = 0; i < 3; i++) {
+        console.log("Conexão feita com sucesso!");
+    }
+    alert("Conexão feita com sucesso!");
+}
+
+imprimeConexão();
