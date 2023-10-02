8c4cbe620b169a353e673f40c239abf5e4d51b36
Vinícius Luiz A <120275756+vlaskepper@users.noreply.github.com>
2023-10-02 13:46:34 -0300

    desenvolvimento-6

---
+++ javascript-I/desenvolvimento-6/script.js
@@ -0,0 +1,8 @@
+function calculadora(num1,num2) {
+    console.log(`${num1} + ${num2} = ${num1+num2}`);
+    console.log(`${num1} - ${num2} = ${num1-num2}`);
+    console.log(`${num1} x ${num2} = ${num1*num2}`);
+    console.log(`${num1} / ${num2} = ${num1/num2}`);
+}
+
+calculadora(4,5);
