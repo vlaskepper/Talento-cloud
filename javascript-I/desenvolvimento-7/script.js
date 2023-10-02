ec37794fc8c8b3fe69119b5413c98d4a5be03afa
Vinícius Luiz A <120275756+vlaskepper@users.noreply.github.com>
2023-10-02 14:16:45 -0300

    desenvolvimento-7

---
+++ javascript-I/desenvolvimento-7/script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+
+body.innerHTML = `
+    <h1 id="titulo">Vende-se está casa</h1>
+    <img src="./img/Casa-Liu-Raiz-Arquitetura-Foto-Leonardo-Giantomasi-2.webp" alt="Imagem da Casa Liu Raiz" class="imagem">
+    <h2>Com vista para o mar, mobiliário orgânico, ambientes integrados e materiais naturais, essa deslumbrante casa de praia brasileira ira te conquistar.</h2>
+    <h3>A Casa Liu Raiz tem:</h3>
+    <ul>
+        <li>Hidromassagem</li>
+        <li>Piscina</li>
+        <li>Vista panorâmica</li>
+    </ul>
+    <p>O preço dessa incrível casa? É R$&infin;&infin;,&infin;&infin;, é super acessível. E você pode pagar em &infin; sem juros.</p>
+`
